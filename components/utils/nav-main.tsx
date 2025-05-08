71f47021e9e72de2423586142636fe77f08ece95
rullyfadheli <rullyfadheli@gmail.com>
2025-05-08 22:47:03 +0700

    Adding sidebar to dashboard page (unfinished)

---
+++ components/utils/nav-main.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { ChevronRight } from "lucide-react";
+
+import Image from "next/image";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url?: string | undefined;
+    icon?: string | undefined;
+    isActive?: boolean;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Main</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            asChild
+            defaultOpen={item.isActive}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild isActive={item?.isActive}>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && (
+                    <Image
+                      src={item.icon}
+                      width={20}
+                      height={20}
+                      alt="sidebar icon"
+                    />
+                  )}
+                  <span>{item.title}</span>
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {item.items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                      <SidebarMenuSubButton asChild>
+                        <a href={subItem.url}>
+                          <span>{subItem.title}</span>
+                        </a>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,21 +5,34 @@ import Card from "@/components/dashboard/Card";
 import Semester from "@/components/dashboard/Semester";
 import EventCalendar from "@/components/dashboard/EventCalendar";
 
+//sidebar
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+import { AppSidebar } from "@/components/utils/app-sidebar";
+
+// import AppSideBar from "@/components/utils/SideBar";
+
 function Dashboard(): JSX.Element {
   return (
-    <div className="w-full pb-4 bg-mainBG">
-      <Banner />
-      <div className="grid h-full lg:grid-cols-2 m-4">
-        <div className="w-full grid grid-cols-1">
-          <PerformanceChart />
-          <Semester />
+    <SidebarProvider>
+      <AppSidebar />
+
+      <main className="w-full h-full pb-4 bg-mainBG">
+        <SidebarTrigger className="absolute top-4 bg-white" />
+        <Banner />
+        <div className="grid h-full lg:grid-cols-2 m-4">
+          <div className="w-full grid grid-cols-1">
+            <PerformanceChart />
+            <Semester />
+          </div>
+          <div className="w-full px-4 pt-2">
+            <Card />
+            <EventCalendar />
+          </div>
         </div>
-        <div className="w-full px-4 pt-2">
-          <Card />
-          <EventCalendar />
-        </div>
-      </div>
-    </div>
+      </main>
+    </SidebarProvider>
   );
 }
 
