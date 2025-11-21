7e045bd4f8a403b29967b3249ecaabf33052b0e9
rullyfadheli <rullyfadheli@gmail.com>
2025-11-21 07:15:06 +0700

    fix: update webpack config to ignore canvas for vercel build

--- next.config.ts
+++ next.config.ts
@@ -15,12 +15,6 @@ const nextConfig: NextConfig = {
   //   domains: ["dzgtfooivmtjsteqttrn.supabase.co"],
   // },
   webpack: (config) => {
-    /**
-     * Configure Webpack to ignore the 'canvas' module.
-     * This is necessary because pdfjs-dist (used by @react-pdf-viewer)
-     * includes a server-side dependency 'canvas' which contains binary files (.node).
-     * These binaries cannot be bundled for the client-side, causing build errors.
-     */
     config.resolve.alias.canvas = false;
 
     // Return the modified config
